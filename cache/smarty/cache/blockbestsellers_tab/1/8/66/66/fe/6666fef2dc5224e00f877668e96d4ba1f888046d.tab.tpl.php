8fbb1e4ca25e01cad1a383c5639cb959f25f5f35
Baltha <adrien.bellonserre@hotmail.fr>
2015-03-28 12:46:38 +0100

    Prestashop Begins

---
+++ cache/smarty/cache/blockbestsellers_tab/1/8/66/66/fe/6666fef2dc5224e00f877668e96d4ba1f888046d.tab.tpl.php
@@ -0,0 +1,19 @@
+<?php /*%%SmartyHeaderCode:2632355101114d32509-27925503%%*/if(!defined('SMARTY_DIR')) exit('no direct access allowed');
+$_valid = $_smarty_tpl->decodeProperties(array (
+  'file_dependency' => 
+  array (
+    '6666fef2dc5224e00f877668e96d4ba1f888046d' => 
+    array (
+      0 => 'C:\\wamp\\www\\prestashop\\themes\\ikon\\modules\\blockbestsellers\\tab.tpl',
+      1 => 1427105504,
+      2 => 'file',
+    ),
+  ),
+  'nocache_hash' => '2632355101114d32509-27925503',
+  'version' => 'Smarty-3.1.19',
+  'unifunc' => 'content_551689323c0013_67369342',
+  'has_nocache_code' => false,
+  'cache_lifetime' => 31536000,
+),true); /*/%%SmartyHeaderCode%%*/?>
+<?php if ($_valid && !is_callable('content_551689323c0013_67369342')) {function content_551689323c0013_67369342($_smarty_tpl) {?>
+<li><a data-toggle="tab" href="#blockbestsellers" class="blockbestsellers">Meilleures Ventes</a></li><?php }} ?>
